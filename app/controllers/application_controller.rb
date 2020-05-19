d835b33e9d634f8f25700c47af3206a450987ad5
icaroquadra <icaroquadra@gmail.com>
2020-05-19 00:05:29 -0300

    relacionamento entre classes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-	include SessionsHelper
+  include SessionsHelper
+  
+  private
+  def require_logged_in_user
+    unless user_signed_in?
+      flash[:danger] = 'Please make login to see the page'
+      redirect_to entrar_path
+    end
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,36 @@
 class UsersController < ApplicationController
+  
+  before_action :require_logged_in_user, only: [:edit, :update]
 
   def new
-    @user = User.new 
+  	@user = User.new
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:succes] = 'Usuário cadastrado com sucesso'
-      redirect_to root_url
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:success] = 'Usuario cadastrado com sucesso'
+  		redirect_to root_url
+  	else
+  	    render 'new'
+  	end
+  end
+  
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      flash[:sucess] = 'Dados atualizados'
+      redirect_to contacts_url
     else
-      render 'new'
+      render 'edit'
     end
   end
 
   private
-  def user_params
-    params.require(:user).permit(:email, :name, :password, :password_confirmation)
-  end
-
+    def user_params
+    	params.require(:user).permit(:email, :name, :password, :password_confirmation)
+    end
+    
 end
